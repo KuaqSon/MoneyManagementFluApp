2033af8640d2f58d017682b8f2260b5cac22babb
Son Nguyen <son@spiderbox.design>
2019-08-08 12:56:18 +0700

    Build api get multiple instagram images using pupeteer

--- ManaMonApi/router/instaDownloader.js
+++ ManaMonApi/router/instaDownloader.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const cheerio = require("cheerio");
 const request = require("request");
+const puppeteer = require("puppeteer");
 
 router.post("/image-urls", function(req, res) {
   const { post_url } = req.body;
@@ -46,6 +47,60 @@ router.post("/image-urls", function(req, res) {
           message: "Cannot load page!"
         });
       }
+    });
+  } else {
+    return res.json({
+      resp: null,
+      isError: true,
+      message: "Invalid post url!"
+    });
+  }
+});
+
+router.get("/image-urls-v2", async function(req, res) {
+  const { post_url } = req.query;
+  if (!post_url) {
+    return res.json({
+      resp: null,
+      isError: true,
+      message: "Link to instagram post cannot blank!"
+    });
+  }
+
+  if (
+    post_url.substring(0, 8) === "https://" ||
+    post_url.substring(0, 7) === "http://"
+  ) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(post_url, { waitUntil: "networkidle0" });
+    await page.waitForFunction("document.querySelectorAll('img').length > 0");
+    const results = await page.evaluate(() => {
+      let elems = [];
+      while (document.querySelectorAll(".coreSpriteRightChevron").length > 0) {
+        let titleLinks = document.querySelectorAll(
+          'img[style="object-fit: cover;"]'
+        );
+        elems = [...elems, ...titleLinks];
+        document.querySelector("button .coreSpriteRightChevron").click();
+      }
+
+      let articles = elems.map(img => ({
+        title: img.getAttribute("alt"),
+        url: img.getAttribute("src")
+      }));
+
+      return articles || [];
+    });
+
+    await browser.close();
+
+    const resp = results.filter(r => r.url !== null);
+
+    return res.json({
+      resp: resp,
+      isError: false,
+      message: "Success!"
     });
   } else {
     return res.json({
